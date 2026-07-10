d2b6438e9e2eedb1eb99b74cf5894d477019d148
ruyana322 <156115962+ruyana322@users.noreply.github.com>
2026-07-10 08:55:19 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.d4nzxml.kythera"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0.0"
     }
